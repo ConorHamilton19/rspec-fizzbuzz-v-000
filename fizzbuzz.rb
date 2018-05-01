967df07d0094058509a71edeb7171cad2f437c7a
Conor Hamilton <conhamil19@gmail.com>
2018-05-01 03:57:37 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,14 +1,14 @@
 def fizzbuzz(int)
-  if int % 3 == 1
+  if int % 3 == 
    puts "Fizz"
    
-  elsif int % 5 == 1
+  elsif int % 5 == 
    puts "Buzz"
    
   elsif int % 5 == 3
    puts "FizzBuzz"
    
- else
-   puts 
-end
+  else
+   
+  end
 end
